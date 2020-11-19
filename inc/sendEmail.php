0c62a4ee4bbd51fc6d530f426b1296f1fdf02a83
LMFonseca <miguel.fonseka@hotmail.com>
2020-11-19 15:43:34 +0000

    email

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -49,7 +49,7 @@ if($_POST) {
       $mail = mail($siteOwnersEmail, $subject, $message, $headers);
 
 		if ($mail) { echo "OK"; }
-      else { echo "Something went wrong. Please try again.vdfvds" + $email; }
+      else { echo "Something went wrong. Please try again."; }
 		
 	} # end if - no validation error
 
